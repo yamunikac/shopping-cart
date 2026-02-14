685d8c3e1fecf80357a0cb4ea206a12bf773c4b9
Chintapanti Yamunika <chinthapantiyamunika1229@gmail.com>
2026-02-14 12:02:50 +0530

    Initial commit - shopping cart project

---
+++ src/data/products.js
@@ -0,0 +1,8 @@
+const products = [
+  { id: 1, name: "iPhone 14", price: 80000 },
+  { id: 2, name: "Laptop", price: 55000 },
+  { id: 3, name: "Headphones", price: 2000 },
+  { id: 4, name: "Smart Watch", price: 5000 }
+]
+
+export default products
